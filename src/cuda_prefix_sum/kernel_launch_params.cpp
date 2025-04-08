e67ea7ef7246388ec2ea2c6247875dbc4789961f
Duane Goodner <dmgoodner@gmail.com>
2025-04-08 17:08:11 -0600

    fix function name in kernel_launch_params.cpp

--- src/cuda_prefix_sum/kernel_launch_params.cpp
+++ src/cuda_prefix_sum/kernel_launch_params.cpp
@@ -2,12 +2,11 @@
 
 #include "common/program_args.hpp"
 
-
-KernelLaunchParams MakeKernelLaunchParams(const ProgramArgs& args) {
-    return KernelLaunchParams{
+KernelLaunchParams CreateKernelLaunchParams(const ProgramArgs &args) {
+  return KernelLaunchParams{
       .full_matrix_dim_x = args.full_matrix_dim()[0],
       .full_matrix_dim_y = args.full_matrix_dim()[1],
       .tile_dim_x = args.tile_dim()[0],
       .tile_dim_y = args.tile_dim()[1],
-    };
-  }
+  };
+}
