d1ef47f9e7701d6fa74d02d9731a27552ee03fbe
Duane Goodner <dmgoodner@gmail.com>
2025-05-19 15:30:13 -0600

    move ArraySize2D operators to array_size_2d.hpp

--- include/common/array_size_2d.hpp
+++ include/common/array_size_2d.hpp
@@ -4,3 +4,12 @@ struct ArraySize2D {
   int num_rows;
   int num_cols;
 };
+
+// Non-member operator==
+inline bool operator==(const ArraySize2D& lhs, const ArraySize2D& rhs) {
+  return lhs.num_rows == rhs.num_rows && lhs.num_cols == rhs.num_cols;
+}
+
+inline bool operator!=(const ArraySize2D& lhs, const ArraySize2D& rhs) {
+  return !(lhs == rhs);
+}
--- include/common/program_args.hpp
+++ include/common/program_args.hpp
@@ -20,14 +20,7 @@
 
 
 
-// Non-member operator==
-inline bool operator==(const ArraySize2D& lhs, const ArraySize2D& rhs) {
-  return lhs.num_rows == rhs.num_rows && lhs.num_cols == rhs.num_cols;
-}
 
-inline bool operator!=(const ArraySize2D& lhs, const ArraySize2D& rhs) {
-  return !(lhs == rhs);
-}
 
 class ProgramArgs {
 public:
